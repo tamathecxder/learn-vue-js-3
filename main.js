1f89f808c624cb3eed5fcb83a3145aac83f6f1c0
tamathecxder <yudistiraeka.pratama012@gmail.com>
2022-08-21 15:30:57 +0700

    HTML Attribute binding and make a data property inside Vue instance

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const product = 'Socks'
+const product = "Socks";
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "Socks",
+      description: "a piece of clothing that is worn on your foot and that covers your ankle and sometimes the lower part of your leg usually plural.",
+      image: "./assets/images/socks_green.jpg",
+      url: "https://www.britanica.com/dictionary/sock",
+    };
+  },
+});
