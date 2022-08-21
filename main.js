16d20d93afe68b39ccfc9bc922bf0f3f60796dbf
tamathecxder <yudistiraeka.pratama012@gmail.com>
2022-08-21 16:23:47 +0700

    Event handling in Vue js and how do we implement it with a methods

--- main.js
+++ main.js
@@ -10,10 +10,33 @@ const app = Vue.createApp({
       url: "https://www.britannica.com/dictionary/sock",
       details: ['50% cotton', '30% wool', '20% polyester'],
       variants: [
-        { id: 1212, color: "Green" },
-        { id: 1213, color: "Blue" }
+        { 
+          id: 1212, 
+          color: "Green",
+          image: "./assets/images/socks_green.jpg"
+        },
+        { 
+          id: 1213, 
+          color: "Blue" ,
+          image: "./assets/images/socks_blue.jpg"
+        }
       ],
       sizes: ["small", "middle", "large", "extra large"],
+      cart: 0,
     };
   },
+  methods: {
+    addToCart(){
+      this.cart += 1;
+    },
+    decrementCart() {
+      if (this.cart <= 0) {
+        return;
+      }
+      this.cart -= 1;
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
+    }
+  }
 });
